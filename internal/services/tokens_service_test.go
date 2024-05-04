9cb916370ee0c805235b6ad54d67fec8bd073889
axdbertuol <axdber@gmail.com>
2024-05-04 19:17:21 -0300

    test: tokens serv

--- internal/services/tokens_service_test.go
+++ internal/services/tokens_service_test.go
@@ -1,25 +1,57 @@
 package services_test
 
-// func TestGenerateTokens(t *testing.T) {
-// 	// Mock arguments for testing
-// 	usr := &dtos.JwtGenDTO{
-// 		UserId:   "123",
-// 		Username: "testuser",
-// 		Role:     "user",
-// 	}
-// 	args := &services.TokensGen{
-// 		AccessToken: &services.TokenArgs{
-// 			User:      usr,
-// 			ExpiresIn: time.Minute * 15,
-// 		},
-// 		RefreshToken: &services.TokenArgs{
-// 			ExpiresIn: time.Hour * 24,
-// 			User:      usr,
-// 		},
-// 	}
+import (
+	"testing"
+	"time"
 
-// 	// accessToken, refreshToken, err := services.GenerateTokens(args, "x")
-// 	assert.NoError(t, err)
-// 	assert.NotEmpty(t, accessToken)
-// 	assert.NotEmpty(t, refreshToken)
-// }
+	"github.com/axdbertuol/auth_service/internal/dtos"
+	"github.com/axdbertuol/auth_service/internal/services"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGenerateTokens(t *testing.T) {
+	// Mock arguments for testing
+	usr := &dtos.JwtGenDTO{
+		UserId:   "123",
+		Username: "testuser",
+		Role:     "user",
+	}
+	args := &services.TokensGen{
+		AccessToken: &services.TokenArgs{
+			User:      usr,
+			ExpiresIn: time.Minute * 15,
+		},
+		RefreshToken: &services.TokenArgs{
+			ExpiresIn: time.Hour * 24,
+			User:      usr,
+		},
+		Secret: "bla",
+	}
+
+	accessToken, refreshToken, err := args.GenerateTokens()
+	assert.NoError(t, err)
+	assert.NotEmpty(t, accessToken)
+	assert.NotEmpty(t, refreshToken)
+}
+func BenchmarkGenerateTokens(b *testing.B) {
+	usr := &dtos.JwtGenDTO{
+		UserId:   "123",
+		Username: "testuser",
+		Role:     "user",
+	}
+	args := &services.TokensGen{
+		AccessToken: &services.TokenArgs{
+			User:      usr,
+			ExpiresIn: time.Minute * 15,
+		},
+		RefreshToken: &services.TokenArgs{
+			ExpiresIn: time.Hour * 24,
+			User:      usr,
+		},
+		Secret: "bla",
+	}
+
+	for i := 0; i < b.N; i++ {
+		_, _, _ = args.GenerateTokens()
+	}
+}
